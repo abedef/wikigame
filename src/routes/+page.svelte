ac47a21bb767a86ed1c23fa52bcb8df24b183355
Abed Fayyad <abed.ef@icloud.com>
2022-11-02 19:26:55 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type { PageServerData } from "./$types";
+
+    export let data: PageServerData;
+</script>
+
+{data.name}
+{data.count}
+<h1>Welcome SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<form action="/">
+    <input type="submit" value="Host" formaction="?/host" />
+    <input type="submit" value="Join" formaction="?/join" />
+</form>
