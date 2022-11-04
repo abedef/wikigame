ebe8672512fc78841eef2ad8bc85084f9d47aac3
Abed Fayyad <abed.ef@icloud.com>
2022-11-04 13:58:52 -0400

    Implement store to persist client game state

---
+++ src/lib/stores.ts
@@ -0,0 +1,28 @@
+import { writable } from "svelte/store";
+import { browser } from '$app/environment';
+
+export enum State {
+    None,
+    Registering,
+    Joining,
+    Joined,
+    Hosting,
+};
+
+const stateKey = "ltmstate";
+const defaultState = { session: "", state: State.None, avatar: "", room: "" };
+
+let state = writable(defaultState);
+if (browser) {
+    const jsonState = localStorage.getItem(stateKey);
+    if (jsonState) {
+        const parsedState = JSON.parse(jsonState);
+        state = writable(parsedState);
+    }
+    state.subscribe((value) => {
+        if (value) localStorage.setItem(stateKey, JSON.stringify(value));
+        else localStorage.removeItem(stateKey);
+    });
+}
+
+export { state };
